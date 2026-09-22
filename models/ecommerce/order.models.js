fb4c573081fdf5b4dbe25d8bbec9eb7f1a26897c
Deepesh Vishwakarma <vishwakarmadeepesh84@gmail.com>
2026-09-22 09:38:21 +0530

    Initial commit

---
+++ models/ecommerce/order.models.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+
+const orderItemsSchema = new mongoose.Schema({
+  productId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Product',
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  }
+});
+const orderSchema = new mongoose.Schema(
+  {
+    orderPrice: {
+      type: Number,
+      required: true,
+    },
+    customer: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+    orderItems: {
+      type: [orderItemsSchema],
+    },
+  },
+  { timestamps: true }
+);
+
+export const Order = mongoose.model('Order', orderSchema);
---
+++ models/ecommerce/product.models.js
@@ -0,0 +1,38 @@
+import mongoose from 'mongoose';
+
+const productSchema = new mongoose.Schema(
+  {
+    description: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    productImage: {
+      type: String,
+    },
+    price: {
+      type: Number,
+      default: 0,
+    },
+    stock: {
+      type: Number,
+      default: 0,
+    },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Category',
+      required: true,
+    },
+    onwer: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Product = mongoose.model('Product', productSchema);
