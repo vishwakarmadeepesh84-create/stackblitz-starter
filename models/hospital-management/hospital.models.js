182a9c41175bb8f3ab8f5d0cc3740f5009bb47d8
vishwakarmadeepesh84-create <vishwakarmadeepesh84@gmail.com>
2026-09-22 17:43:07 +0530

    Create models schema in hospital-mangement file

--- models/hospital-management/hospital.models.js
+++ models/hospital-management/hospital.models.js
@@ -2,8 +2,32 @@ import mongoose from "mongoose";
 
 const hospitalSchema = new mongoose.Schema(
     {
+        name: {
+            type: String,
+            required: true,
+        },
+        addressLine1: {
+            type: String,
+            required: true,
+        },
+        addressLine2: {
+            type: String,
+        },
+        city: {
+            type: String,
+            required: true,
+        },
+        pincode: {
+            type: String,
+            required: true,
+        },
+        specializedIn: [
+            {
+                type: String,
+            }
+        ]
 
     },
-    {timestamps: true});
+    { timestamps: true });
 
-    export const Hospital = mongoose.model("Hospital", hospitalSchema);
+export const Hospital = mongoose.model("Hospital", hospitalSchema);
